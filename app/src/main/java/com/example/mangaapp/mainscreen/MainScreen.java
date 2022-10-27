78f08421338ab3ca4666700dd3eebccb3882d97c
Halcyon1902 <70284297+halcyon1902@users.noreply.github.com>
2022-10-27 23:25:49 +0700

    4
    _tạo form đăng nhập
    _test kiểm tra tài khoản và mật khẩu để đăng nhập

--- app/src/main/java/com/example/mangaapp/mainscreen/MainScreen.java
+++ app/src/main/java/com/example/mangaapp/mainscreen/MainScreen.java
@@ -1,37 +1,81 @@
 package com.example.mangaapp.mainscreen;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.mangaapp.R;
-import com.example.mangaapp.menu.MenuActivity;
+import com.example.mangaapp.function.Login;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainScreen extends AppCompatActivity {
-    ImageView menu;
+public class MainScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Full màn hình
+        setFullScreen();
+        setContentView(R.layout.drawer_layout);
+        init();
+        initNavigationDrawer();
+        //
+
+    }
+
+    //Full màn hình
+    private void setFullScreen() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.main_screen);
-        init();
-        //Điều hướng thanh menu
-        menu.setOnClickListener(v -> {
-            startActivity(new Intent(this, MenuActivity.class));
-            finish();
-        });
     }
 
     //Khởi tạo
     public void init() {
-        menu = findViewById(R.id.iv_menu);
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
     }
 
+    private void initNavigationDrawer() {
+        navigationView.setNavigationItemSelectedListener(this);
+        //tạo menu mở drawer trên toolbar
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColor(getColor(R.color.goldenrod));
+        navigationView.setItemIconTintList(null);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.account:
+                startActivity(new Intent(this, Login.class));
+                finish();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/mangaapp/model/TaiKhoan.java
@@ -0,0 +1,90 @@
+package com.example.mangaapp.model;
+
+public class TaiKhoan {
+    private String ID;
+    private String TaiKhoan;
+    private String MatKhau;
+    private String Email;
+    private boolean PhanQuyen;
+    private boolean TrangThai;
+    private String[] BinhLuans;
+    private String NgayTao;
+
+    public TaiKhoan() {
+    }
+
+    public TaiKhoan(String ID, String taiKhoan, String matKhau, String email, boolean phanQuyen, boolean trangThai, String[] binhLuans, String ngayTao) {
+        this.ID = ID;
+        TaiKhoan = taiKhoan;
+        MatKhau = matKhau;
+        Email = email;
+        PhanQuyen = phanQuyen;
+        TrangThai = trangThai;
+        BinhLuans = binhLuans;
+        NgayTao = ngayTao;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTaiKhoan() {
+        return TaiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        TaiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        MatKhau = matKhau;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public boolean isPhanQuyen() {
+        return PhanQuyen;
+    }
+
+    public void setPhanQuyen(boolean phanQuyen) {
+        PhanQuyen = phanQuyen;
+    }
+
+    public boolean isTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        TrangThai = trangThai;
+    }
+
+    public String[] getBinhLuans() {
+        return BinhLuans;
+    }
+
+    public void setBinhLuans(String[] binhLuans) {
+        BinhLuans = binhLuans;
+    }
+
+    public String getNgayTao() {
+        return NgayTao;
+    }
+
+    public void setNgayTao(String ngayTao) {
+        NgayTao = ngayTao;
+    }
+}
